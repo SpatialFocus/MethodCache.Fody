9baf8ec9ab3b1013dfd49f4620d6b019e8e235c9
Christopher Dresel <christopher.dresel@spatial-focus.net>
2020-11-02 08:22:34 +0100

    Corrected project guids, added sample project

---
+++ src/SpatialFocus.MethodCache.Fody.Sample/IDistributedCacheObjectFormatter.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ICacheObjectFormatter.cs" company="Spatial Focus GmbH">
+// Copyright (c) Spatial Focus GmbH. All rights reserved.
+// </copyright>
+
+namespace SpatialFocus.MethodCache.Fody.Sample
+{
+	public interface IDistributedCacheObjectFormatter
+	{
+		public T Deserialize<T>(byte[] data);
+
+		public byte[] Serialize<T>(T value);
+	}
+}
---
+++ src/SpatialFocus.MethodCache.Fody.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="Spatial Focus GmbH">
+// Copyright (c) Spatial Focus GmbH. All rights reserved.
+// </copyright>
+
+namespace SpatialFocus.MethodCache.Fody.Sample
+{
+	using System;
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
